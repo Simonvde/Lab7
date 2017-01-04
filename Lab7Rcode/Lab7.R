05558edbaa5432510017b77e6cff0ca96f4e4f38
martirm <marti.renedo@gmail.com>
2017-01-04 11:35:24 +0100

    fixed function "spread"

--- Lab7Rcode/Lab7.R
+++ Lab7Rcode/Lab7.R
@@ -13,13 +13,16 @@ spread <- function(M,v,tmax,beta,gamma){
   A <- matrix(nrow=tmax,ncol=n)
   A[1,] <- as.logical(v)
   for(i in 2:tmax){
-    n_inf <- M%*%v  #a vector containing the number of infected neighbours of each node
+    n_inf <- M%*%as.numeric(A[i-1,])  #a vector containing the number of infected neighbours of each node
     inf <- n_inf > rgeom(n,beta)
     cured <- as.logical(rbinom(n=n,size=1,prob=gamma)*A[i-1,]) #vector infected of nodes which are cured (with probability gamma)
     A[i,] <- as.logical((A[i-1,] | inf) & !cured) #an element is infected if it already was or gets infected this time step, and it hasn't been cured
+    
   }
   return (A)
 }
+
+
 
 # Graphs ------------------------------------------------------------------
 n <- 2000
